492b7732b2474eab932a3eb90098bbb411134460
harshalprasad <harshalprasad324@gmail.com>
2026-08-09 15:32:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (48.44%) - LeetSync

--- 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.cpp
+++ 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.cpp
@@ -1,31 +1,11 @@
 class Solution {
 public:
     int minOperations(int n) {
+        int mid = n/2;
 
-        vector<int>nums(n);
-
-        for(int i=0; i<n; i++)
-        {
-            nums[i] = (2*i) + 1;
-        }
-
-        int i=0; 
-        int j=n-1;
-        int opern=0;
-
-        while(i<j)
-        {
-            while(nums[i] != nums[j])
-            {
-                nums[i]++;
-                nums[j]--;
-                opern++;
-            }
-            i++;
-            j--;
-        }
-
-        return opern;
-        
+        if(n%2 != 0)
+            return mid*(mid+1);
+        else
+            return mid*mid;
     }
 };
