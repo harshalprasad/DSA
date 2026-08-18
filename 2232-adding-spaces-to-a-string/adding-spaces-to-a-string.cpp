d8faf79d7484bd9f96dbfeb2f73e385f8422b8ff
harshalprasad <harshalprasad324@gmail.com>
2026-08-18 17:42:23 +0530

    Time: 13 ms (85.82%) | Memory: 85.6 MB (60.55%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
@@ -2,26 +2,19 @@ class Solution {
 public:
     string addSpaces(string s, vector<int>& spaces) {
         string ans;
-        int m=0;
         int j=0;
 
-        for(int i=0; i<spaces.size(); i++)
+        for(int i=0; i<s.size(); i++)
         {
-            int k = spaces[i] - m;
-            
-            while(k)
+            if(j<spaces.size() && i == spaces[j] )
             {
-                ans += s[j++];
-                k--;
+                ans += " ";
+                j++;
             }
-            ans += " ";
-            m = spaces[i];
+
+            ans += s[i];
         }
 
-        while(j<s.size())
-        {
-            ans += s[j++];
-        }
 
         return ans;
         
