fe2e0789257bc197f7872877b3c6cebee7890ade
harshalprasad <harshalprasad324@gmail.com>
2026-08-27 01:24:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (18.12%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cpp
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int balancedStringSplit(string s) {
-        int count_l = 0;
-        int count_r = 0;
         int substring = 0;
-        string ans;
+        int balancetracker = 0;
 
         for(int i=0; i<s.size(); i++)
         {
-            ans += s[i];
             if(s[i] == 'L')
-            count_l++;
-            else 
-            count_r++;
+            balancetracker++;
+            else
+            balancetracker--;
 
-            if(count_l == count_r)
-            {
-                substring++;
-                ans = "";
-            }
+            if(balancetracker == 0)
+            substring++;
         }
 
         return substring;
