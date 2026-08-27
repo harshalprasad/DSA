bc72be9eac5708196adc4dc2b6d332e69524944c
harshalprasad <harshalprasad324@gmail.com>
2026-08-27 20:05:33 +0530

    Time: 0 ms (100.00%) | Memory: 32.4 MB (78.85%) - LeetSync

---
+++ 4058-compute-alternating-sum/compute-alternating-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int alternatingSum(vector<int>& nums) {
+        
+        int sumeven = 0, sumodd = 0;
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(i%2 == 0)
+            sumeven += nums[i];
+            else
+            sumodd += nums[i];
+        }
+
+        return sumeven - sumodd;
+    }
+};
